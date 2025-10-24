c9a3af9ff376cc789b4f5e9a4ae2c18f88bb79e2
boullle <bill.bouilly1@gmail.com>
2025-10-24 17:03:19 +0200

    tp04 a-p corrections

--- tp04.sql
+++ tp04.sql
@@ -17,7 +17,7 @@ WHERE a.designation like '%sachet%';
 --e
 select * 
 from article as a 
-WHERE upper(a.designation) like upper('%sachet%');
+WHERE upper(a.designation) like '%SACHET%';
 --f
 SELECT * 
 FROM article 
@@ -29,11 +29,11 @@ FROM article
 JOIN fournisseur ON article.id_fou = fournisseur.id 
 WHERE fournisseur.nom = 'Dubois & Fils';
 --h
-SELECT * 
+SELECT * AVG(article.prix) as prix_moyen, fournisseur.nom 
 FROM article JOIN fournisseur ON article.id_fou = fournisseur.id 
 WHERE fournisseur.nom = 'Dubois & Fils';
 --i
-SELECT AVG(article.prix), fournisseur.nom 
+SELECT AVG(article.prix) as prix_moyen, fournisseur.nom 
 FROM article JOIN fournisseur ON article.id_fou = fournisseur.id 
 GROUP BY fournisseur.nom;
 --j
@@ -45,8 +45,7 @@ SELECT b.id,b.numero,b.date_cmde,b.delai
 FROM compo as c 
 inner join article as a on c.id_art = a.id 
 inner join bon as b on c.id_bon=b.id 
-WHERE a.designation like 'Boulon%' 
-order by b.id;
+WHERE a.designation like 'Boulon%';
 --l
 SELECT b.id,b.numero,b.date_cmde,b.delai,f.nom 
 FROM compo as c 
@@ -54,34 +53,28 @@ inner join article as a on c.id_art = a.id
 inner join bon as b on c.id_bon=b.id
 inner join fournisseur as f on a.id_fou=f.id
 WHERE a.designation like 'Boulon%' 
-order by b.id;
 --m
-SELECT b.id,b.numero,b.date_cmde,b.delai,f.nom, sum(c.qte*a.prix)  as 'prix du bon de commande'
+SELECT b.id,b.numero,b.date_cmde,b.delai, sum(c.qte*a.prix)  as 'prix du bon de commande'
 FROM compo as c 
 inner join article as a on c.id_art = a.id 
 inner join bon as b on c.id_bon=b.id
-inner join fournisseur as f on a.id_fou=f.id
 group by b.id;
 --n
-SELECT b.id,b.numero,b.date_cmde,b.delai,f.nom, sum(c.qte)  as 'prix du bon de commande'
+SELECT b.id,b.numero,b.date_cmde,b.delai, sum(c.qte)  as 'prix du bon de commande'
 FROM compo as c 
-inner join article as a on c.id_art = a.id 
 inner join bon as b on c.id_bon=b.id
-inner join fournisseur as f on a.id_fou=f.id
 group by b.id;
 --o
-SELECT b.id,b.numero,b.date_cmde,b.delai,f.nom, sum(c.qte)  as 'qte'
+SELECT b.id, sum(c.qte)  as 'qte'
 FROM compo as c 
-inner join article as a on c.id_art = a.id 
 inner join bon as b on c.id_bon=b.id
-inner join fournisseur as f on a.id_fou=f.id
 group by b.id
 having qte>25;
 --p
 SELECT sum(c.qte*a.prix)  as 'prix du bon de commande'
 FROM compo as c 
 inner join article as a on c.id_art = a.id 
-right join bon as b on c.id_bon=b.id
+join bon as b on c.id_bon=b.id
 where b.date_cmde between '2019-04-01' and  '2019-04-30';
 --q
 
